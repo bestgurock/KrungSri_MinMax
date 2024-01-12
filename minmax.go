09995687e199aff108d1acea97a0852a865dd321
BAYAD\11474004 <chonlasit.tawornying@krungsri.com>
2024-01-13 01:10:12 +0700

    refactor: delete all duplicate code and only return the first position of sorted data

--- minmax.go
+++ minmax.go
@@ -3,20 +3,6 @@ package main
 import "sort"
 
 func Min(data []float64) float64 {
-	if data[0] == 7 {
-		return 7
-	} else if data[0] == 2 {
-		return 2
-	} else if data[0] == 1 {
-		return 1
-	}
 	sort.Float64s(data)
-	if data[0] == 5 {
-		return 5
-	} else if data[0] == 12 {
-		return 12
-	} else if data[0] == -2 {
-		return -2
-	}
-	return 0
+	return data[0]
 }
